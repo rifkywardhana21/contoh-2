1eb463d29981b6ec605b45242a0fc735ba17c549
Muhammad Rifky Wardhana <152211890+rifkywardhana21@users.noreply.github.com>
2026-03-12 11:02:23 +0700

    project tes_1

--- Contoh_2.py
+++ Contoh_2.py
@@ -1,41 +1,41 @@
-# Project Array + Class Python
-# Kelas X RPL 2
+nama_siswa = []
+nilai_siswa = []
+pilihan = 0
 
-class Siswa:
-    def __init__(self):
-        self.nama_siswa = []
-        self.nilai_siswa = []
+print("=== APLIKASI DATA & NILAI SISWA ===")
 
-    def tambah_siswa(self, nama, nilai):
-        self.nama_siswa.append(nama)
-        self.nilai_siswa.append(nilai)
+while pilihan != 5:
+    print("\nMENU")
+    print("1. Tambah Data Siswa & Nilai")
+    print("2. Tampilkan Data")
+    print("3. Hitung Rata-rata Nilai")
+    print("4. Hitung Jumlah Siswa")
+    print("5. Keluar")
 
-    def tampilkan_siswa(self):
-        print("\nDaftar Data Siswa:")
-        for i in range(len(self.nama_siswa)):
-            print(f"Siswa ke-{i+1}: {self.nama_siswa[i]} | Nilai: {self.nilai_siswa[i]}")
+    pilihan = int(input("Pilih menu (1-5): "))
 
-    def jumlah_siswa(self):
-        print("\nJumlah siswa:", len(self.nama_siswa))
+    if pilihan == 1:
+        jumlah = int(input("Masukkan jumlah siswa: "))
+        for i in range(jumlah):
+            nama = input(f"Masukkan nama siswa ke-{i+1}: ")
+            nilai = int(input(f"Masukkan nilai siswa ke-{i+1}: "))
+            nama_siswa.append(nama)
+            nilai_siswa.append(nilai)
 
-    def rata_rata_nilai(self):
-        total = sum(self.nilai_siswa)
-        rata_rata = total / len(self.nilai_siswa)
-        print("Rata-rata nilai siswa:", rata_rata)
+    elif pilihan == 2:
+        # TODO: tampilkan nama dan nilai siswa
+        pass
 
+    elif pilihan == 3:
+        # TODO: hitung dan tampilkan rata-rata nilai
+        pass
 
+    elif pilihan == 4:
+        # TODO: tampilkan jumlah siswa
+        pass
 
-print("=== SISTEM DATA SISWA ===")
+    elif pilihan == 5:
+        print("Program selesai")
 
-data = Siswa()
-
-jumlah = int(input("Masukkan jumlah siswa: "))
-
-for i in range(jumlah):
-    nama = input(f"Masukkan nama siswa ke-{i+1}: ")
-    nilai = int(input(f"Masukkan nilai siswa ke-{i+1}: "))
-    data.tambah_siswa(nama, nilai)
-
-data.tampilkan_siswa()
-data.jumlah_siswa()
-data.rata_rata_nilai()
+    else:
+        print("Menu tidak tersedia")
